0bf3854d8e870226c7d476b0184dd20f229f743f
Tobias Burdow [Kaleidox] <kaleidox@comroid.org>
2023-12-10 11:00:03 +0100

    stuff

--- fancyworldname.js
+++ fancyworldname.js
@@ -13,7 +13,7 @@ function $() {
         case 'atf10/DIM-1': return '&cN'; // Nether
         case 'atf10/DIM1': return '&eE'; // the End
         case 'atf10/aether/the_aether': return '&3A'; // Aether
-        case 'atf10/bloodmagic/dungeon': return '&9D'; // Dungeon
+        case 'atf10/bloodmagic/dungeon': return '&9X'; // Dungeon
         case 'atf10/blue_skies/everdawn': return '&0D'; // everDawn
         case 'atf10/blue_skies/everbright': return '&0B'; // everBright
         case 'atf10/twilightforest/twilight_forest': return '&5F'; // twilight Forest
