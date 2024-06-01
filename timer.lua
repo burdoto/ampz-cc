2f37d76ae57baa225dea34a87c73a4dc3de721b3
burdoto <kaleidox@comroid.org>
2024-06-01 10:34:24 +0200

    fix formatting

--- timer.lua
+++ timer.lua
@@ -1,19 +1,19 @@
- monitor = peripheral.find("monitor");
- timer = 0;
- timerId = 0;
+monitor = peripheral.find("monitor");
+timer = 0;
+timerId = 0;
 
- outputSide = arg[1];
- inverse = arg[2] == "-i";
+outputSide = arg[1];
+inverse = arg[2] == "-i";
 
- monitor.clear();
- timerId = os.startTimer(1);
- while true do
-     -- calculate positions
-     width, height = monitor.getSize();
-     lineRemaining = math.floor(height * 0.4);
-     lineRemSpace = math.floor(width * 0.142857143);
-     lineBtnAdd30 = math.floor(height * 0.8);
-     lineBA30Space = math.floor(width * 0.256);
+monitor.clear();
+timerId = os.startTimer(1);
+while true do
+    -- calculate positions
+    width, height = monitor.getSize();
+    lineRemaining = math.floor(height * 0.4);
+    lineRemSpace = math.floor(width * 0.142857143);
+    lineBtnAdd30 = math.floor(height * 0.8);
+    lineBA30Space = math.floor(width * 0.256);
     lineBtnCancel = height;
     lineBtCclSpace = math.floor(width * 0.33);
 
@@ -25,35 +25,36 @@
 
     -- print +time button
     monitor.setCursorPos(lineBA30Space + 1, lineBtnAdd30);
-    monitor.blit("+5min", "00000","55555");
+    monitor.blit("+5min", "00000", "55555");
 
     -- print cancel button
     monitor.setCursorPos(lineBtCclSpace + 1, lineBtnCancel);
-    monitor.blit("Stop", "0000","eeee");
+    monitor.blit("Stop", "0000", "eeee");
 
-    repeat event, id, x, y = os.pullEventRaw()
-    -- debug print(event); if id ~= nil then print(id) end
+    repeat
+        event, id, x, y = os.pullEventRaw()
+        -- debug print(event); if id ~= nil then print(id) end
     until event == "timer" or event == "monitor_touch";
     if event == "timer" and id == timerId then
-    -- timer elapse
-    if timer > 0 then
-        timer = timer - 1;
-    end
-    timerId = os.startTimer(1);
+        -- timer elapse
+        if timer > 0 then
+            timer = timer - 1;
+        end
+        timerId = os.startTimer(1);
     elseif (event == "monitor_touch") then
-    -- button interact
-    --print("touch["..x..","..y.."]")
+        -- button interact
+        --print("touch["..x..","..y.."]")
 
-    if y == lineBtnAdd30 and x > lineBA30Space and x < (lineBtnAdd30 + 3) then
-        -- +30sec button
-        timer = timer + 300;
-    elseif y == lineBtnCancel and x > lineBtCclSpace and x < (lineBtCclSpace + 5) then
-        -- cancel button
-        timer = 0;
+        if y == lineBtnAdd30 and x > lineBA30Space and x < (lineBtnAdd30 + 3) then
+            -- +30sec button
+            timer = timer + 300;
+        elseif y == lineBtnCancel and x > lineBtCclSpace and x < (lineBtCclSpace + 5) then
+            -- cancel button
+            timer = 0;
+        end
     end
-end
---print(timer);
-result = timer > 0;
-if inverse then result = not result end
-redstone.setOutput(outputSide, result);
+    --print(timer);
+    result = timer > 0;
+    if inverse then result = not result end
+    redstone.setOutput(outputSide, result);
 end
